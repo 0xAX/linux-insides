ffd1a82c8d323bfdd4935dbccf2e4a338f54e152
Alexander Kuleshov <kuleshovmail@gmail.com>
2025-08-25 22:56:13 +0500

    scripts: ignore github directory
    This commit adds .github directory to exclusion to not check it in CI job.
    
    Signed-off-by: Alexander Kuleshov <kuleshovmail@gmail.com>

--- scripts/check-code-snippets.py
+++ scripts/check-code-snippets.py
@@ -1,3 +1,7 @@
+"""
+A script that takes the lines of the Linux kernel source code from the comments
+in the markdown files that are attached to the code and checks their validity.
+"""
 import os
 import re
 import sys
@@ -5,26 +9,61 @@ from typing import Optional, Tuple
 
 import requests
 
-def split_url_and_range(url: str) -> Tuple[str, Optional[int], Optional[int]]:
+exclude_dirs = ["./.github"]
+
+def __split_url_and_range__(url: str) -> Tuple[str, Optional[int], Optional[int]]:
     base, frag = url.split("#", 1)
     m = re.match(r'L(\d+)(?:-L?(\d+))?$', frag)
     start = int(m.group(1))
     end = int(m.group(2)) if m.group(2) else None
     return base, start, end
 
-def fetch_raw(source: str) -> str:
-    r = requests.get(source)
+def __fetch_raw__(source: str) -> str:
+    r = requests.get(source, timeout=5.0)
     return r.text
 
-def main():
+def __handle_md__(md: str):
     in_code = False
     code = ''
     content = ''
-    start_line = 0
-    end_line = 0
+
+    md_lines = md.splitlines()
+
+    for line in md_lines:
+        if in_code:
+            if re.search("^```[a-zA-Z].*", line):
+                continue
+
+            if re.search("^```$", line):
+                in_code = False
+                continue
+
+            code += line + '\n'
+            continue
+
+        if line.startswith("<!--"):
+            in_code = True
+            (uri, start, end) = __split_url_and_range__(line.split(' ')[1])
+            content = "\n".join(__fetch_raw__(uri).splitlines()[start-1:end]).rstrip()
+            continue
+
+        if code != '':
+            if code.rstrip() != content:
+                print("Error in", sys.argv[1])
+                print("Code in book:")
+                print(code)
+                print("Code from github:")
+                print(content)
+                sys.exit(1)
+
+            code = ''
+            content = ''
+            continue
+
+def __main__():
     md_files = []
-    
-    for root, dirs, files in os.walk(sys.argv[1]):
+
+    for root, _dirs, files in os.walk(sys.argv[1]):
         for name in files:
             if name.endswith('.md'):
                 md_files.append(os.path.join(root, name))
@@ -33,41 +72,13 @@ def main():
 
     for md in md_files:
         print("Checking code in the", md)
+        if os.path.dirname(md) in exclude_dirs:
+            continue
 
         with open(md, "r", encoding="utf-8") as f:
             md = f.read()
 
-        md_lines = md.splitlines()
-        for line in md_lines:
-            if in_code:
-                if re.search("^```[a-zA-Z].*", line):
-                    continue
-
-                if re.search("^```$", line):
-                    in_code = False
-                    continue
-
-                code += line + '\n'
-                continue
-
-            if line.startswith("<!--"):
-                in_code = True
-                (uri, start, end) = split_url_and_range(line.split(' ')[1])
-                content = "\n".join(fetch_raw(uri).splitlines()[start-1:end]).rstrip()
-                continue
-
-            if code != '':
-                if code.rstrip() != content:
-                    print("Error in", sys.argv[1])
-                    print("Code in book:")
-                    print(code)
-                    print("Code from github:")
-                    print(content)
-                    exit(1)
-
-                code = ''
-                content = ''
-                continue
+        __handle_md__(md)
 
 if __name__ == "__main__":
-    main()
+    __main__()
